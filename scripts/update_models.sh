4f9395ae5dc88775be0049a0fd0240b9b6ef07b0
Joe Goldbeck <joegoldbeck@users.noreply.github.com>
2019-11-06 01:42:16 -0500

    Add bigram matching algo

--- scripts/update_models.sh
+++ scripts/update_models.sh
@@ -4,3 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 python $DIR/update_gensim_models_v1.py
 python $DIR/update_gensim_models_v2.py
+python $DIR/update_gensim_models_v3.py
